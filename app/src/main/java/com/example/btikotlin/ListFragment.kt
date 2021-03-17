1bfccd13b4de055603c730f39f8956dcc299636e
IvandPdD <ivan_de_prada_apps1ta1920@cev.com>
2021-03-17 07:16:04 +0100

    Dynamic Search

--- app/src/main/java/com/example/btikotlin/ListFragment.kt
+++ app/src/main/java/com/example/btikotlin/ListFragment.kt
@@ -6,10 +6,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.appcompat.widget.SearchView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class ListFragment : Fragment() {
+
+    val foodArray = arrayOf("Potasio25mg", "Calorías 100mg", "Proteínas 200mg", "Hidratos de Carbono 67g")
+
+    var foodList:MutableList<Food> = ArrayList()
+    var displayList:MutableList<Food> = ArrayList()
+
+    var adapter = SearchAdapter()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -18,34 +27,60 @@ class ListFragment : Fragment() {
         
         val view = inflater.inflate(R.layout.fragment_list, container, false)
 
+        loadData()
+
         val recycler = view.findViewById<RecyclerView>(R.id.searchRecycler)
         recycler.layoutManager = LinearLayoutManager(requireActivity())
-        val adapter = SearchAdapter()
         recycler.adapter = adapter
 
         adapter.onItemClickListener = {
             Toast.makeText(requireActivity(), it.name, Toast.LENGTH_SHORT).show()
         }
 
-        val foodArray = arrayOf("Potasio25mg", "Calorías 100mg", "Proteínas 200mg", "Hidratos de Carbono 67g")
+        adapter.submitList(displayList)
 
-        val foodList = mutableListOf(
-            Food("Manzana", foodArray),
-            Food("Melón", foodArray),
-            Food("Sandía",foodArray),
-            Food("Chocolate",foodArray),
-            Food("Plátano",foodArray),
-            Food("Lentejas",foodArray),
-            Food("Pollo",foodArray),
-            Food("Granada",foodArray),
-            Food("Patata",foodArray),
-            Food("Huevo",foodArray),
-            Food("Jamón",foodArray),
-            Food("Bacalao",foodArray),
-        )
+        val searchBar  = view.findViewById<SearchView>(R.id.searchView)
+        searchBar.setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return true
+            }
 
-        adapter.submitList(foodList)
+            override fun onQueryTextChange(newText: String?): Boolean {
+
+                if (newText!!.isNotEmpty()){
+                    displayList.clear()
+                    val search = newText.toLowerCase()
+                    foodList.forEach{
+                        if (it.name.toLowerCase().contains(search)){
+                            displayList.add(it)
+                        }
+                    }
+                    adapter.notifyDataSetChanged()
+                }else{
+                    displayList.clear()
+                    adapter.notifyDataSetChanged()
+                }
+
+                return true
+            }
+
+        })
 
         return view
+    }
+
+    fun loadData() {
+        foodList.add(Food("Manzana", foodArray))
+        foodList.add(Food("Melón", foodArray))
+        foodList.add(Food("Sandía", foodArray))
+        foodList.add(Food("Chocolate", foodArray))
+        foodList.add(Food("Plátano", foodArray))
+        foodList.add(Food("Lentejas", foodArray))
+        foodList.add(Food("Pollo", foodArray))
+        foodList.add(Food("Granada", foodArray))
+        foodList.add(Food("Patata", foodArray))
+        foodList.add(Food("Huevo", foodArray))
+        foodList.add(Food("Jamón", foodArray))
+        foodList.add(Food("Bacalao", foodArray))
     }
 }
